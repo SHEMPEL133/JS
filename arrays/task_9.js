adcdd7d2b8463b1193d948884778224cf7ef499d
Denis <den.shempel@gmail.com>
2019-07-25 20:12:41 +0300

    task_9

---
+++ arrays/task_9.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function (arr) {
+    var count = 0;
+    len = arr.length;
+    var i = 0;  
+    while(i < len){
+        if (arr[i] == 0) {
+            arr.splice(i, 0, 0);
+            count++;
+            i++;
+        }
+        i++;
+    }
+    arr.splice(len, count);
+};
+
