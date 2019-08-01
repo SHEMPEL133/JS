c12e139d56dfb40a336d28c564db3d6d98484d31
Denis <den.shempel@gmail.com>
2019-08-01 14:19:42 +0300

    task_7

---
+++ object/task_7.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    var obj = {};
+    for(var i = 0; i<nums.length; i++){
+        if(obj.hasOwnProperty(nums[i])){
+            return true;
+        }else{
+            obj[nums[i]] = 1;
+        }
+    }
+    return false;
+};
